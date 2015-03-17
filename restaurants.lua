789e23857ee77118423cfbf3f1e41992cc501649
Alexander McGilvray <alex@levelism.com>
2015-03-17 05:39:10 -0700

    added gps coordinates for existing restaurant data

--- restaurants.lua
+++ restaurants.lua
@@ -11,63 +11,81 @@ function initRestaurants()
   rTheHub = {
     name = "The Hub",
     distanceInBlocks = 1,
-    score = 50 
+    score = 50,
+    longitude = 49.275173,
+    latitude = -123.121676
   }
   table.insert(restaurants,rTheHub) 
   --Salsa y Agave
   rSalsaAndAgave = {
     name = "Salsa y Agave",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.274146,
+    latitude = -123.122423
   }
   table.insert(restaurants,rSalsaAndAgave) 
   --Phat Sandwiches 
   rPhatSandwiches = {
     name = "Phat Sandwiches",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.276275,
+    latitude = -123.120011
   }
   table.insert(restaurants,rPhatSandwiches) 
   --The Parlour
   rTheParlour = {
     name = "The Parlour",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.276882,
+    latitude = -123.120154
   }
   table.insert(restaurants,rTheParlour) 
   --Alpha Sushi
   rAlphaSushi = {
     name = "Alpha Sushi",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.276747,
+    latitude = -123.127486
   }
   table.insert(restaurants,rAlphaSushi) 
   --Ki Issu (check the spelling on this)
   rKiIssu = {
     name = "Ki Issu Sushi",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.273889,
+    latitude = -123.123475
   }
   table.insert(restaurants,rKiIssu) 
   --Hurricane Grill
   rHurricaneGrill = {
     name = "Hurricane Grill",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.273745,
+    latitude = -123.119271
   }
   table.insert(restaurants,rHurricaneGrill) 
   --Earls
   rEarls = {
     name = "Earls",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.275941,
+    latitude = -123.120547
   }
   table.insert(restaurants,rHurricaneGrill) 
   --Five guys
   rFiveGuys= {
     name = "Five Guys",
     distanceInBlocks = 2,
-    score = 50 
+    score = 50,
+    longitude = 49.281261,
+    latitude = -123.119167
   }
   table.insert(restaurants,rFiveGuys)  
 end
