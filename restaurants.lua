1eca13651f263b2f614e713c76dc178d45fcdfdf
Alexander McGilvray <alex@levelism.com>
2015-03-25 21:09:05 -0700

    fixed up the lat long data, had it backwards

--- restaurants.lua
+++ restaurants.lua
@@ -14,8 +14,8 @@ function initRestaurants()
     name = "The Hub",
     distanceInBlocks = 1,
     score = 50,
-    longitude = 49.275173,
-    latitude = -123.121676
+    latitude = 49.275173,
+    longitude = -123.121676
   }
   table.insert(restaurants,rTheHub) 
   --Salsa y Agave
@@ -23,8 +23,8 @@ function initRestaurants()
     name = "Salsa y Agave",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.274146,
-    latitude = -123.122423
+    latitude = 49.274146,
+    longitude = -123.122423
   }
   table.insert(restaurants,rSalsaAndAgave) 
   --Phat Sandwiches 
@@ -32,8 +32,8 @@ function initRestaurants()
     name = "Phat Sandwiches",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.276275,
-    latitude = -123.120011
+    latitude = 49.276275,
+    longitude = -123.120011
   }
   table.insert(restaurants,rPhatSandwiches) 
   --The Parlour
@@ -41,8 +41,8 @@ function initRestaurants()
     name = "The Parlour",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.276882,
-    latitude = -123.120154
+    latitude = 49.276882,
+    longitude = -123.120154
   }
   table.insert(restaurants,rTheParlour) 
   --Alpha Sushi
@@ -50,8 +50,8 @@ function initRestaurants()
     name = "Alpha Sushi",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.276747,
-    latitude = -123.127486
+    latitude = 49.276747,
+    longitude = -123.127486
   }
   table.insert(restaurants,rAlphaSushi) 
   --Ki Issu (check the spelling on this)
@@ -59,8 +59,8 @@ function initRestaurants()
     name = "Ki Issu Sushi",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.273889,
-    latitude = -123.123475
+    latitude = 49.273889,
+    longitude = -123.123475
   }
   table.insert(restaurants,rKiIssu) 
   --Hurricane Grill
@@ -68,8 +68,8 @@ function initRestaurants()
     name = "Hurricane Grill",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.273745,
-    latitude = -123.119271
+    latitude = 49.273745,
+    longitude = -123.119271
   }
   table.insert(restaurants,rHurricaneGrill) 
   --Five guys
@@ -77,8 +77,8 @@ function initRestaurants()
     name = "Five Guys",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.281261,
-    latitude = -123.119167
+    latitude = 49.281261,
+    longitude = -123.119167
   }
   table.insert(restaurants,rFiveGuys) 
   --Earls
@@ -86,8 +86,8 @@ function initRestaurants()
     name = "Earls",
     distanceInBlocks = 2,
     score = 50,
-    longitude = 49.275941,
-    latitude = -123.120547
+    latitude = 49.275941,
+    longitude = -123.120547
   }
   table.insert(restaurants,rHurricaneGrill)  
 end
